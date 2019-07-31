0269365ca4304e4f9fc21ec87c638a4f47e3ec30
Milankril <52549387+Milankril@users.noreply.github.com>
2019-07-31 13:41:32 +1200

    Add files via upload
    Code complete with background and working on buttons

---
+++ Beach defender.py
@@ -0,0 +1,63 @@
+import pygame
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((520, 676)) # Background position
+
+pygame.display.set_caption("Beach Defender")
+
+bg = pygame.image.load('bg.png') # Background Image
+
+clock = pygame.time.Clock()
+
+green = (0,200,0)
+red = (200,0,0)
+
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
+def redrawGameWindow():
+    screen.fill((255, 255, 255))
+    screen.blit(bg, (0, 0))
+
+    mouse = pygame.mouse.get_pos()
+
+    print(mouse)
+
+    if 100+125 > mouse [0] > 100 and 478+50 > mouse [1] > 478:
+        pygame.draw.rect(screen, green, (100,478,125,50))
+    else:
+        pygame.draw.rect(screen, bright_green, (100,478,125,50))
+
+
+
+
+    if 300+125 >  mouse [0] > 300 and 478+50 > mouse [1] > 478:
+        pygame.draw.rect(screen, red, (300,478,125,50))
+    else:
+        pygame.draw.rect(screen, bright_red, (300,478,125,50))
+
+
+
+
+    pygame.display.update()
+
+font 
+run = True
+while run:
+    clock.tick(30)
+
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            run = False
+
+    redrawGameWindow()
+
+
+pygame.quit()
+
+
+
+
