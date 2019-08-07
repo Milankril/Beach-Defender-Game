2ec63a3b59b483b48eceb92c04e06da3c2d638da
Milankril <52549387+Milankril@users.noreply.github.com>
2019-08-08 11:51:46 +1200

    Beach defender 1.2
    This has working buttons for 'play' and 'quit'

--- Beach defender.py
+++ Beach defender.py
@@ -3,68 +3,92 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((520, 676)) # Background position
+screenwidth = 520
+screenheight = 676
+
+screen = pygame.display.set_mode((screenwidth, screenheight)) # Background position
 
 pygame.display.set_caption("Beach Defender")
 
 bg = pygame.image.load('bg.png') # Background Image
+br = pygame.image.load('')
+x = (screenwidth*0.45)
+y = (screenheight*0.8)
 
 clock = pygame.time.Clock()
 
 green = (0,200,0)
 red = (200,0,0)
+play = ''
+
 
 bright_red = (255,0,0)
 bright_green = (0,255,0)
 
-def button():
-     mouse = pygame.mouse.get_pos()
-
-     if 100+125 > mouse [0] > 100 and 478+50 > mouse [1] > 478:
-        pygame.draw.rect(screen, green, (100,478,125,50))
-     else:
-        pygame.draw.rect(screen, bright_green, (100,478,125,50))
-     play = font.render('PLAY', 1, ((0, 0, 0)))
-     screen.blit(play, (100, 485))
-
-
-
-     if 300+125 > mouse [0] > 300 and 478+50 > mouse [1] > 478:
-        pygame.draw.rect(screen, red, (300,478,125,50))
-     else:
-        pygame.draw.rect(screen, bright_red, (300,478,125,50))
-     Quit = font.render('QUIT', 1, ((0, 0, 0)))
-     screen.blit(Quit, (300, 485))
-
-def redrawGameWindow():
-    screen.fill((255, 255, 255))
-    screen.blit(bg, (0, 0))
-
-
+def button(action=''):
+    global scrn, play, run
     mouse = pygame.mouse.get_pos()
-
+    click = pygame.mouse.get_pressed()
     if 100+125 > mouse [0] > 100 and 478+50 > mouse [1] > 478:
         pygame.draw.rect(screen, green, (100,478,125,50))
-    else:
+        if click[0] == 1 and action == 'clicked':
+            print("cancan")
+            scrn = 1
+            print(scrn)
+
+    elif scrn == 0:
         pygame.draw.rect(screen, bright_green, (100,478,125,50))
     play = font.render('PLAY', 1, ((0, 0, 0)))
     screen.blit(play, (100, 485))
 
 
-
-    if 300+125 >  mouse [0] > 300 and 478+50 > mouse [1] > 478:
+    if 300+125 > mouse [0] > 300 and 478+50 > mouse [1] > 478:
         pygame.draw.rect(screen, red, (300,478,125,50))
-    else:
+        if click[0] == 1 and action == 'pressed':
+            run = False
+    elif scrn == 0:
         pygame.draw.rect(screen, bright_red, (300,478,125,50))
     Quit = font.render('QUIT', 1, ((0, 0, 0)))
     screen.blit(Quit, (300, 485))
 
+def redrawGameWindow():
+    global scrn
+    screen.fill((255, 255, 255))
+    if scrn == 0:
+        screen.blit(bg, (0, 0))
 
 
+        mouse = pygame.mouse.get_pos()
 
-    pygame.display.update()
+        '''
+        if 100+125 > mouse [0] > 100 and 478+50 > mouse [1] > 478:
+            pygame.draw.rect(screen, green, (100,478,125,50))
+        else:
+            pygame.draw.rect(screen, bright_green, (100,478,125,50))
+        play = font.render('PLAY', 1, ((0, 0, 0)))
+        screen.blit(play, (100, 485))
+        '''
+        button('clicked')
+
+        '''
+        if 300+125 >  mouse [0] > 300 and 478+50 > mouse [1] > 478:
+            pygame.draw.rect(screen, red, (300,478,125,50))
+        else:
+            pygame.draw.rect(screen, bright_red, (300,478,125,50))
+        Quit = font.render('QUIT', 1, ((0, 0, 0)))
+        screen.blit(Quit, (300, 485))
+        '''
+
+        button('pressed')
+        pygame.display.update()
+
+
+    if scrn == 1:
+        screen.fill((255, 100, 100))
+        pygame.display.update()
 
 font = pygame.font.SysFont('', 60, True)
+scrn = 0
 run = True
 while run:
     clock.tick(30)
